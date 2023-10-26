2ac02e8c2852a491769539fff0725739196ed60e
SaadSamir7 <saadsamir7177@gmail.com>
2023-10-26 05:03:21 +0300

    Python - Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -18,3 +18,10 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
