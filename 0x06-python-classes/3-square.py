9100b67715e1a2c516dc22ba28c2c1d6504fcea6
SaadSamir7 <saadsamir7177@gmail.com>
2023-10-26 05:17:27 +0300

    Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,12 +8,20 @@ area method that returns the area of the square.
 
 class Square:
     """Defines a square."""
-    def __init(self, size=0):
-        if not isinstance(size, int):
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
