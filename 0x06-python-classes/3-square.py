1df5128debf534325d756054ca963304cfd2461f
SaadSamir7 <saadsamir7177@gmail.com>
2023-10-26 05:15:14 +0300

    Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square and init method that
+sets its size and checking if the given values are right. There's also an
+area method that returns the area of the square.
+"""
+
 
 class Square:
+    """Defines a square."""
     def __init(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
