e0e5e18c30e33a82e8dd3741430a57dc8bb3b60b
SaadSamir7 <saadsamir7177@gmail.com>
2023-10-31 00:59:38 +0200

    Python - More Classes and Objects

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,58 @@
+import sys
+
+
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, len(board))):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(n):
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    sol = []
+
+    def solve(row):
+        if row == n:
+            sol.append([(i, row.index(1)) for i, row in enumerate(board)])
+            return
+
+        for col in range(n):
+            if is_safe(board, row, col):
+                board[row][col] = 1
+                solve(row + 1)
+                board[row][col] = 0
+
+    solve(0)
+    for solution in sol:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_nqueens(n)
