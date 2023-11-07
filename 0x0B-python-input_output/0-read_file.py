e3fc2e89489388b9ab1eeeaaaee5f7e54dfac32e
SaadSamir7 <saadsamir7177@gmail.com>
2023-11-07 21:54:53 +0200

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
+
 def read_file(filename=""):
-    """Read and print the content of a text file to stdout.
-
-    Args:
-        filename (str): the text file to read (default empty string).
-
-    Note:
-        This func doesnt handle file permission or exceptions.
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
