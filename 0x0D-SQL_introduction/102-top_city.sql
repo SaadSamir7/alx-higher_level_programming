d25faeba2e95ba7f30222ed545eb9d682a12fa45
SaadSamir7 <saadsamir7177@gmail.com>
2023-12-15 02:19:02 +0200

    SQL - Introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,9 @@
 -- 102-top_city.sql
 
 -- Display the top 3 cities' temperatures during July and August, ordered by temperature (descending)
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperature_data
-WHERE MONTH(date) IN (7, 8) -- Filter for July and August
-GROUP BY city
-ORDER BY avg_temp DESC
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- 103-max_state.sql
 
 -- Display the max temperature of each state, ordered by State name
-SELECT state, MAX(temperature) AS max_temp
-FROM temperature_data
-GROUP BY state
-ORDER BY state;
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
